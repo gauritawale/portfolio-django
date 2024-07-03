7c39f52558792e7c2fd23959fea529776e7e8b71
Gauri Tawale <113706508+gauritawale@users.noreply.github.com>
2024-07-03 19:41:51 +0530

    Add navbar to html file
    include navbar in html file

--- Portfolio/portfolio/portfolio/settings.py
+++ Portfolio/portfolio/portfolio/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-yf7u8hpti5nqm+-0alkxo9i)zu$&)=)4ur*l!7u07j9neo#42h
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [ ]
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
